177e96b27a13fee7ded1a620acd92251051db014
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-09-15 14:45:29 +0300

    Install socket on server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
-const http = require('http');
-const app = require('./app');
+const http = require("http");
+const { Server } = require("socket.io");
+const app = require("./app");
 
 const PORT = process.env.PORT ?? 5000;
 
 const httpServer = http.createServer(app);
+
+const io = new Server(httpServer);
+
+io.on("connection", (socket) => {
+  console.log("Connected established", socket);
+});
 
 httpServer.listen(PORT, () => {
   console.log(`Server is running!`);
