7072307e573865704184eb283a2e0eac245a511f
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-09-15 15:46:57 +0300

    Install socket on client

--- server/index.js
+++ server/index.js
@@ -6,10 +6,13 @@ const PORT = process.env.PORT ?? 5000;
 
 const httpServer = http.createServer(app);
 
-const io = new Server(httpServer);
+const io = new Server(httpServer, { cors: { origin: "*" } });
 
 io.on("connection", (socket) => {
   console.log("Connected established", socket);
+  socket.emit("HELLO_SELF", "Hello on our service");
+  socket.broadcast.emit("NEW_ANOTHER_SOCKET", "New socked was added");
+  io.emit('TO_EVERYONE','Service will reboot at 5 minut')
 });
 
 httpServer.listen(PORT, () => {
